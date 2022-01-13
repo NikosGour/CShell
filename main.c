ce25e82102ff823ffdf83803c372dd919fa0ca41
Ledrake <nikosgournak@gmail.com>
2022-01-14 01:30:14 +0200

    Linux

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
 
 int main(int argc, char const *argv[])
 {
-    printf("Nikos"); 
-    printf("\n");
+    char *buf;
+    buf=(char *)malloc(10*sizeof(char));
+    buf=getlogin(); 
+  
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
+    printf("%s:%s>\n",buf, cwd);
     return 0;
 }

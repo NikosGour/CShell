5c2e6f780cadb7661c43f4617772946a1e98610f
NikosGour <nikosgournak@gmail.com>
2022-01-14 03:32:47 +0200

    Latest

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ A Custom C Shell for Linux
 
 ## By
 - Nikos Gournakis
+
+### Quick Start
+
+```shell
+gcc main.c -o main && ./main
+```
--- main.c
+++ main.c
@@ -1,15 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<unistd.h>
+#include<sys/types.h>
+#include<pwd.h>
+
+#pragma region "Global Variables"
+char current_working_directory[1024];
+char current_user[1024];
+#pragma endregion
+
+#pragma region get_current user/directory
+void update_current_user()
+{
+    struct passwd *pw = getpwuid(getuid());
+    char *homedir = pw->pw_name;
+    strcpy(current_user, homedir);
+}
+void update_current_working_dir()
+{
+    char* pwd = getcwd(current_working_directory, sizeof(current_working_directory));
+    if(pwd == NULL)
+    {
+        perror("getcwd error");
+        exit(1);
+    }
+    
+}
+#pragma endregion
 
 int main(int argc, char const *argv[])
 {
-    char *buf;
-    buf=(char *)malloc(10*sizeof(char));
-    buf=getlogin(); 
-  
-    char cwd[1024];
-    getcwd(cwd, sizeof(cwd));
-    printf("%s:%s>\n",buf, cwd);
+    struct passwd* x = getpwuid(getuid()); 
+    update_current_user();
+    update_current_working_dir();
+    printf("%s:%s>\n",current_user, current_working_directory);
     return 0;
 }
